1c3cfe3109a4b7886beb4f4de18abed8aa303c83
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-06-24 23:07:44 +0800

    feat: add color output matrix

---
+++ engine/include/kivo/cinema/output/color_output_matrix.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+struct ColorOutputMatrix {
+  std::string id;
+  std::string input_signal;
+  std::string output_path;
+  std::string claim_level;
+  std::string quality_tier;
+};
